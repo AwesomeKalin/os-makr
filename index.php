c46fa843883e92af0aca1c707805a657b2e6664f
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2019-02-24 11:14:08 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id" content="1094422425831-beb29glhadm2nmfl1vkiq1uqni5gh9uo.apps.googleusercontent.com">
   </head>
   <body>
+    <h1>Login with Google:</h1>
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
   </body>
 </html>
